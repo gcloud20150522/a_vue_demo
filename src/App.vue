4c38b694ce0ca8c5ba0ed4c5abb583da0af20bda
gcloud20150522 <gcloud20150522@icloud.com>
2019-05-10 17:09:40 +0800

    a vue demo

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <header class="site-header jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1>请发表对Vue的评论</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      <Add @refresh="refresh"/>
+      <List :comments="comments"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Add from './components/add'
+import List from './components/list'
+
+
+export default {
+  name: 'App',
+  components: {
+    Add,List
+  },
+  data(){
+    return{
+      comments:[
+        {name:'詹姆斯.哈登',content:'我是大胡子，我是mvp'},
+        {name:'克里斯.保罗',content:'我是谁？我是cp3'},
+        {name:'阿伦.艾佛森',content:'我只有183，我却无人能挡'}
+      ],
+    }
+  },
+  methods:{
+    refresh(name,content){
+      var comment={name,content};
+
+      this.comments.push(comment);
+    },
+    
+  },
+  // mouted(){
+  //   console.log(this)
+  // }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/list.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <div class="col-md-8">
+    <h3 class="reply">评论回复：</h3>
+    <h2 v-show="comments.length===0">暂无评论，点击左侧添加评论！！！</h2>
+    <ul class="list-group">
+      <Item v-for="(comment,index) in comments" :key="index" :comment="comment" @removeItem="removeItem(index)"/>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import Item from './item'
+  export default {
+    components:{
+      Item,
+    },
+    props:['comments'],
+    methods:{
+      removeItem(index){
+        this.comments.splice(index,1)
+      }
+    }
+  }
+</script>
+
+<style lang="css" scoped>
+  .reply {
+    margin-top: 0px;
+  }
+
+
+</style>
